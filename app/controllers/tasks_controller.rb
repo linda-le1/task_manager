7fbc745b76b0375bcb2e221774c227355f1d813c
Linda Le <yueling1222@gmail.com>
2019-11-16 13:42:47 -0700

    Initial commit with half of project complete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to '/tasks'
+  end
+
+end
